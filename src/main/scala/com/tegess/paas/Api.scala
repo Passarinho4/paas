f4c7db4236c6520fa6439e48968f8588d8dd6b82
Passarinho4 <passarinho4@gmail.com>
2017-11-22 20:46:51 +0100

    Instance id endpoint, change port to 80

--- src/main/scala/com/tegess/paas/Api.scala
+++ src/main/scala/com/tegess/paas/Api.scala
@@ -1,12 +1,21 @@
 package com.tegess.paas
 
+import java.util.UUID
+
 import org.springframework.web.bind.annotation.{GetMapping, RestController}
 
 @RestController
 class Api {
 
+  val someRandomValue = UUID.randomUUID().toString
+
   @GetMapping(path = Array("/"))
   def get() = {
    "...............…………………………._¸„„„„_ </br>…………………….…………...„--~*'¯…….'\\ </br>………….…………………… („-~~--„¸_….,/ì'Ì </br>…….…………………….¸„-^\"¯ : : : : :¸-¯\"¯/' </br>……………………¸„„-^\"¯ : : : : : : : '\\¸„„,-\" </br>**¯¯¯'^^~-„„„----~^*'\"¯ : : : : : : : : : :¸-\" </br>.:.:.:.:.„-^\" : : : : : : : : : : : : : : : : :„-\" </br>:.:.:.:.:.:.:.:.:.:.: : : : : : : : : : ¸„-^¯ </br>.::.:.:.:.:.:.:.:. : : : : : : : ¸„„-^¯ </br>:.' : : '\\ : : : : : : : ;¸„„-~\" </br>:.:.:: :\"-„\"\"***/*'ì¸'¯ </br>:.': : : : :\"-„ : : :\"\\ </br>.:.:.: : : : :\" : : : : \\, </br>:.: : : : : : : : : : : : 'Ì </br>: : : : : : :, : : : : : :/ </br>\"-„_::::_„-*__„„~\""
+  }
+
+  @GetMapping(path = Array("/info"))
+  def info() = {
+    s"Instance id: $someRandomValue"
   }
 }
